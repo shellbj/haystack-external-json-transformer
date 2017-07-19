6ae7c98a8522ef16031e5f912468534e76e58728
jeffbaker <jeffbaker@expedia.com>
2017-07-19 09:20:16 -0700

    Add dependencies on haystack-idl, kafka_2.11, kafka-streams, protobuf-java-util. Add code in App.java to show that the generated classes from haystack-idl are visible. Move App.java and AppTest.java to a new Java package com.expedia.www.haystack.external.

---
+++ src/main/java/com/expedia/www/haystack/external/App.java
@@ -0,0 +1,16 @@
+package com.expedia.www.haystack.external;
+
+import com.expedia.open.tracing.Span;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Prove that the generated code from haystack-idl is visible to this package
+        Span span = null;
+    }
+}
--- src/test/java/com/expedia/www/haystack/external/AppTest.java
+++ src/test/java/com/expedia/www/haystack/external/AppTest.java
@@ -1,4 +1,4 @@
-package com.expedia.www.haystack;
+package com.expedia.www.haystack.external;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
