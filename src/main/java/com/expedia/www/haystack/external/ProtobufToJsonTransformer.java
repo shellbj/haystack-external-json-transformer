63d6720e363f537e0f5b58f1f306ab178aa47edd
jeffbaker <jeffbaker@expedia.com>
2017-08-02 14:38:31 -0700

    Add unit tests for ProtobufToJsonTransformer. Added Factory to ProtobufToJsonTransformer to facilitate unit testing.

--- src/main/java/com/expedia/www/haystack/external/ProtobufToJsonTransformer.java
+++ src/main/java/com/expedia/www/haystack/external/ProtobufToJsonTransformer.java
@@ -2,6 +2,7 @@ package com.expedia.www.haystack.external;
 
 import com.expedia.open.tracing.Span;
 import org.apache.commons.text.StrSubstitutor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -17,21 +18,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
-// TODO Write a test to exercise this code (probably an integration test)
 public class ProtobufToJsonTransformer {
 
-    final static String CLIENT_ID = "External";
+    static final String CLIENT_ID = "External";
+    static final String STARTED_MSG = "Now started ScanStream";
+
+    static Factory factory = new Factory(); // will be mocked out in unit tests
     static Logger logger = LoggerFactory.getLogger(ProtobufToJsonTransformer.class);
 
     // TODO Move topics to a centralized location to be used by all services
-    private final static String KAFKA_FROM_TOPIC = "SpanObject-ProtobufFormat-Topic-1";
-    private final static String KAFKA_TO_TOPIC = "SpanObject-JsonFormat-Topic-3";
+    static final String KAFKA_FROM_TOPIC = "SpanObject-ProtobufFormat-Topic-1";
+    static final String KAFKA_TO_TOPIC = "SpanObject-JsonFormat-Topic-3";
 
-    private final static String KLASS_NAME = ProtobufToJsonTransformer.class.getName();
-    private final static String KLASS_SIMPLE_NAME = ProtobufToJsonTransformer.class.getSimpleName();
+    static final String KLASS_NAME = ProtobufToJsonTransformer.class.getName();
+    static final String KLASS_SIMPLE_NAME = ProtobufToJsonTransformer.class.getSimpleName();
 
     public static void main(String[] args) {
         final SpanProtobufDeserializer protobufDeserializer = new SpanProtobufDeserializer();
@@ -39,35 +42,43 @@ public class ProtobufToJsonTransformer {
         final Serde<Span> spanSerde = Serdes.serdeFrom(spanJsonSerializer, protobufDeserializer);
         final Serde<String> stringSerde = Serdes.String();
 
-        final KStreamBuilder kStreamBuilder = new KStreamBuilder();
+        final KStreamBuilder kStreamBuilder = factory.createKStreamBuilder();
         final KStream<String, Span> stream = kStreamBuilder.stream(stringSerde, spanSerde, KAFKA_FROM_TOPIC);
         stream.mapValues(span -> Span.newBuilder(span).build()).to(stringSerde, spanSerde, KAFKA_TO_TOPIC);
 
         final StreamsConfig streamsConfig = new StreamsConfig(getProperties());
-        final KafkaStreams kafkaStreams = new KafkaStreams(kStreamBuilder, streamsConfig);
+        final KafkaStreams kafkaStreams = factory.createKafkaStreams(kStreamBuilder, streamsConfig);
         kafkaStreams.start();
-        logger.info("Now started ScanStream");
+        logger.info(STARTED_MSG);
     }
 
-    // TODO Read configurations from a file with environment-specific values
-    private static Properties getProperties() {
+    static Properties getProperties() {
         final Properties props = new Properties();
         props.put(StreamsConfig.CLIENT_ID_CONFIG, CLIENT_ID);
-        props.put("group.id", KLASS_NAME);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, KLASS_NAME);
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, KLASS_SIMPLE_NAME);
-        final String kafkaIpAndPort = getKafkaIpAnPort();
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaIpAndPort);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, getKafkaIpAnPort());
         props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 1);
         props.put(StreamsConfig.TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class);
         return props;
     }
 
     private static String getKafkaIpAnPort() {
-        // TODO Add EnvironmentVariablesConfigurationSource object first in list for env variable from apply-compose.sh
-        final ConfigFilesProvider cfp = () -> Arrays.asList(Paths.get("base.yaml"));
+        // TODO Add EnvironmentVariablesConfigurationSource object to handle env variables from apply-compose.sh et al
+        final ConfigFilesProvider cfp = () -> Collections.singletonList(Paths.get("base.yaml"));
         final ClasspathConfigurationSource ccs = new ClasspathConfigurationSource(cfp);
         final ConfigurationProvider cp = new ConfigurationProviderBuilder().withConfigurationSource(ccs).build();
         final KafkaConfig kafkaConfig = cp.bind("haystack.kafka", KafkaConfig.class);
         return StrSubstitutor.replaceSystemProperties(kafkaConfig.brokers()) + ":" + kafkaConfig.port();
+    }
+
+    static class Factory {
+        KStreamBuilder createKStreamBuilder() {
+            return new KStreamBuilder();
+        }
+
+        KafkaStreams createKafkaStreams(KStreamBuilder kStreamBuilder, StreamsConfig streamsConfig) {
+            return new KafkaStreams(kStreamBuilder, streamsConfig);
+        }
     }
 }
